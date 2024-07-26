351fa26e7d301546bbd208275fe707cb0dbf0a13
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-07-26 17:27:24 +0530

    commit

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,18 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
+public static void main (String args[]){
+    int a=100;
+    int count=0;
+    for (int i=1;i<=100;i++){
+        if(a%i==0){
+            count++;
+
+        }
+    }
+        if (count==2){
+            System.out.println("prime number");
+        }else{
+            System.out.println("Not a prime");
+        }
+    }
 }
