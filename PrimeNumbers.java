4921d51f112c34691c1543814d4852ef2ec51434
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-26 13:28:50 +0530

    commit

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class PrimeNumbers{
 // complete the Prime Number Logic
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter number");
+    int num=sc.nextInt();
+    // int count=0;
+    for(int i=1;i<=num;i++){//i=3,i=4
+        int count=0; //to reset the value of count
+        for(int j=1;j<=i;j++){//1<=3,1<=4
+            if(i%j==0){//3%1=0,3%2==1 not,3%3=0  4%1=0
+                count++; //+1              +=2    +1=3
+            }
+        }
+      if(count==2){
+        System.out.println(i+ " is prime");
+      } 
+ 
+    }
+    
+    
+}
 }
