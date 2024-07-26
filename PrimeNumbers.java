d11d0ff93e81d711ed429267267989d63b6b6f3b
sujatha-94 <sujathakarri701@gmail.com>
2024-07-26 13:12:38 +0530

    prime

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class PrimeNumbers{
 // complete the Prime Number Logic
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a=s.nextInt();
+        if(a<=1){
+            System.out.println("not a prime num");
+        }
+        
+        int count = 0;
+        for (int j = 1; j <= a; j++) { 
+             if (a % j == 0) {
+                count++;
+            }
+        }
+       if (count == 2) {
+           System.out.println("prime num");
+       } else {
+           System.out.println("not a prime num");
+       }
+   }  
 }
+
