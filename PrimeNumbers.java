7c1489c9a6fc5de99738a42528f3db56c85e6a9f
Verat-hub <venkey482thalka@gmail.com>
2024-07-26 12:55:38 +0530

    Venkat

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class PrimeNumbers{
 // complete the Prime Number Logic
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int num=scan.nextInt();
+    int count=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("prime Number");
+        }
+        else{
+            System.out.println("Composite Number");
+        }
+        count=0;
+    }
 }
+
