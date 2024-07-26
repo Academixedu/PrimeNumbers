d4bf2dcd36e86ee477615b6481aa7a479b0f603d
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-26 13:03:24 +0530

    changes

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,18 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
+    public static void main(String args[]){
+        //1-100//
+        
+        for(int j=1;j<=100;j++){
+            int count=0;
+            for(int i=1;i<=j;i++){
+                if(j%i==0){
+                   count++;
+                }
+            }
+            if(count==2){
+                System.out.println(j+" is a prime number");
+            }
+        }
+    }
 }
