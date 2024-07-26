96dfb2ac6a10f83a1fd6e12ffa384c8a9dd80191
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-26 19:11:18 +0530

    sarvesh

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,20 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
+public static void main(String[] args) {
+     int a=11;
+    int count=0;
+        
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("this is prime num");
+        }else{
+            System.out.println("not prime num");
+        }
+        
+    }
 }
+
