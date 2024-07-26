f9af76b02d894a718a4820c4376d635cfa4b8994
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-26 12:53:54 +0530

    goutham

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,18 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
+public static void main(String[] args) {
+    int a = 10;
+    int count = 0;
+    for(int i=1;i<=a;i++){
+        if(a%2==0){
+            count++;
+        }
+    }
+    if(count==0){
+        System.out.println("prime");
+    }
+    else{
+        System.out.println("not prime");
+    }
+}
 }
