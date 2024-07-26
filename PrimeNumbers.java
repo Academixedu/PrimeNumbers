82347c329fcc5d339763e849c20a2560f1d3b11e
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-26 16:57:33 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,24 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
+
+public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=in.nextInt();
+        int count=0;
+        for(int i=1;i<=num;i++){
+        
+        if(num%i==0){
+            count++;
+        }
+
+    }
+    if(count==2){
+        System.out.println("prime");
+    }
+     else {
+        System.out.println("not prime");
+     }
+    
+    }
 }
