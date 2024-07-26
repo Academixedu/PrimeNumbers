c58c3a6f9cbe1f7b798942227f0dd250d59a037e
psvsai <pasupuletisvsm245@gmail.com>
2024-07-26 16:45:57 +0530

    psvsai

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,19 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
-}
+public static void main(String[] args) {
+    int a =100;
+    int count = 0;
+    for (int j=1; j<=100; j++){
+          for (int i=1; i<=j; i++){
+                if (j%i ==0){
+                  count++;
+         }
+      }
+         if (count==2){
+          System.out.println(j);
+      }
+     count=0;
+    
+   }
+   }  
+}  
