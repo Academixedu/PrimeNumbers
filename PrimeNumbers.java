d4bff7e1cfbc9b60ef124fd7fd9ac84bf2798b3a
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-28 16:53:37 +0530

    Abhishek

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,3 +1,27 @@
 public class PrimeNumbers{
 // complete the Prime Number Logic
-}
+    public static void main(String[] args)
+     {
+        int a=100;
+        int count=0;
+        for(int i=1;i<=100;i++)
+        {
+            if(a%i==0)
+            {
+                count++;
+
+            }
+
+        }
+        if(count==2)
+        {
+            System.out.println("Prime number");
+        }
+        else
+        {
+            System.out.println("NO prime number ");
+        }
+    }
+
+     }
+
